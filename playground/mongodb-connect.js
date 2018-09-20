4f23635da306a3c5202d9851337c95af484d0672
Swapnil8074 <swapnilshukla201@gmail.com>
2018-09-21 01:53:51 +0530

    Playground completed for mongodb native driver

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,6 +1,6 @@
-let mongoClient = require("mongodb").MongoClient;
+let { MongoClient, ObjectID } = require("mongodb");
 
-mongoClient.connect(
+MongoClient.connect(
   "mongodb://localhost:27017/TodoApp",
   { useNewUrlParser: true },
   (err, client) => {
@@ -23,13 +23,45 @@ mongoClient.connect(
     //   }
     // );
 
-    // finding document in mongodb
+    // // finding document in mongodb
+    // db.collection("Users")
+    //   .find({ name: "Swapnil" })
+    //   .toArray()
+    //   .then(docs => {
+    //     console.log(docs);
+    //   })
+    //   .catch(err => console.log(err));
+
+    // // deleting user using deleteMany
+    // db.collection("Users")
+    //   .deleteMany({ name: "Swapnil" })
+    //   .then(res => {
+    //     console.log(res.result);
+    //   })
+    //   .catch(err => console.log(err));
+
+    // // deleteing user using findOneAndDelete
+    // db.collection("Users")
+    //   .findOneAndDelete({ _id: new ObjectID("5ba3f720a9bd2835bc4df276") })
+    //   .then(result => {
+    //     console.log(result);
+    //   })
+    //   .catch(err => console.log(err));
+
+    // // updating document using findOneAndUpdate
     db.collection("Users")
-      .find({ name: "Swapnil" })
-      .toArray()
-      .then(docs => {
-        console.log(docs);
-      })
+      .findOneAndUpdate(
+        { _id: new ObjectID("5ba3f70c8b583c2984fa80be") },
+        {
+          $inc: {
+            age: 1
+          }
+        },
+        {
+          returnOriginal: false
+        }
+      )
+      .then(res => console.log(res))
       .catch(err => console.log(err));
 
     client.close();
